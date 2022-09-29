635a7fe186015c8b4bd9ece4f0bcb0ff19eb6e07
Stephen Buys <stephenbuys404@gmail.com>
2022-09-29 13:14:50 +0200

    2nd Project

---
+++ reactshoppingcartapp/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header(props) {
+
+    const {countCartItems}=props;
+
+    return (<header className="row block center"><div>
+        <a href="#/cart">Cart{' '}{countCartItems? (<button className="badge">{countCartItems}</button>):('')}</a>{' '}
+        <a href="#/signin">SignIn</a>
+    </div></header>
+    );
+}
