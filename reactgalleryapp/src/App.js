2cfabb55ed0dddc36b3f3199542a96dacb18298e
Stephen Buys <stephenbuys404@gmail.com>
2022-09-29 13:11:38 +0200

    1st Project

---
+++ reactgalleryapp/src/App.js
@@ -0,0 +1,12 @@
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Gallery from "./portfolio-gallery/Gallery";
+import React from "react";
+
+class App extends React.Component {
+  render(){
+  return (
+    <Gallery />
+  );
+  }
+}
+export default App;
---
+++ reactgalleryapp/src/portfolio-gallery/Items.js
@@ -0,0 +1,21 @@
+function Items({data}){
+    return (
+        <div className="row ">
+        {
+          data.map((value)=>{
+           const {id, image, title, description} = value;
+           return (
+            <div className="col-sm-3 my-3" key={id}>
+             <div className="card bg-light text-center">
+                 <img alt="" src={image} className="img-fluid" style={{height:"200px"}}/>
+                 <h3>{title}</h3>
+                 <p>{description}</p>
+            </div>
+         </div>
+           )
+          })
+        }
+</div>
+    )
+}
+export default Items;
